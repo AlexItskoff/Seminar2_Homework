e581b99531e0b7adf6a1bb4ba0adacc7a7dd9b8f
AlexItskoff <119017869+AlexItskoff@users.noreply.github.com>
2022-12-06 19:00:54 +0300

    Add files via upload

---
+++ Test_15.cs
@@ -0,0 +1,20 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру,
+ //обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+
+Console.WriteLine("Введите число от 1 до 7");
+
+ int number1 = int.Parse(Console.ReadLine()!);
+ 
+ if (number1 >0 && number1 < 6) 
+ Console.WriteLine("NO");
+
+  else 
+  Console.WriteLine("YES!");
+
+
+
